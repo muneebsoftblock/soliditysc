c8ab7a8b9051d4bebc922733a022ac69cbbaf4cd
Muneeb <muneeb.softblock@gmail.com>
2022-07-11 08:55:58 +0500

    testing pass

--- test/moon_hedgehog.js
+++ test/moon_hedgehog.js
@@ -1,8 +1,26 @@
 const MoonHedgehog = artifacts.require('MoonHedgehog');
 
-contract('MoonHedgehog', ([acc1, acc2, acc3]) => {
+contract('MoonHedgehog', ([alice, bob, carol, owner]) => {
   it('should assert true', async () => {
-    await MoonHedgehog.deployed();
+    const sc = await MoonHedgehog.new({ from: owner });
+
+    const scOwner = await sc.owner();
+    const scAddr = sc.address;
+    console.log({ scOwner, scAddr });
+
+    await sc.setSaleActiveTime(0, 0, { from: owner });
+    await sc.setFirstFreeMints(5, { from: owner });
+
+    {
+      const qty = 3;
+      const price = '' + (await sc.getPrice(qty, { from: bob }));
+      console.log({ price });
+      await sc.buyHedgehog(qty, {
+        from: bob,
+        value: price,
+      });
+    }
+
     return assert.isTrue(true);
   });
 });
