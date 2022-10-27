73e200e86efcb1967a0c35b1286a3e1aedd51a7e
Muneeb <muneeb.softblock@gmail.com>
2022-10-28 00:08:12 +0500

    init

--- contracts/Morph.sol
+++ contracts/Morph.sol
@@ -1,275 +1,147 @@
+// Crypto Kings Club
+// 10,000 Kings are Invading the Metaverse to takeover their throne as the rightful rulers
+
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.14;
-
-import "erc721a/contracts/ERC721A.sol";
-import "erc721a/contracts/extensions/ERC721ABurnable.sol";
-import "erc721a/contracts/extensions/ERC721AQueryable.sol";
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
+import "erc721a@3.3.0/contracts/ERC721A.sol";
+import "erc721a@3.3.0/contracts/extensions/ERC721ABurnable.sol";
+import "erc721a@3.3.0/contracts/extensions/ERC721AQueryable.sol";
 
-contract MoonHedgehogsSale is
-    ERC721A("Moon Hedgehogs", "MH"),
-    Ownable,
-    ERC721AQueryable,
-    ERC721ABurnable,
-    ERC2981
-{
-    // Variables
+contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryable, ERC721ABurnable, ERC2981, Ownable, ReentrancyGuard {
+    // Main Sale
+    uint256 public kingPrice = 0.35 ether;
     uint256 public constant maxSupply = 10000;
-    uint256 public reservedHedgehogs = 500;
-
-    uint256 public freeHedgehogs = 0;
-    uint256 public freeMaxHedgehogsPerWallet = 0;
-    uint256 public freeSaleActiveTime = type(uint256).max;
-
-    uint256 public firstFreeMints = 1;
-    uint256 public maxHedgehogsPerWallet = 2;
-    uint256 public hedgehogsPrice = 0.01 ether;
     uint256 public saleActiveTime = type(uint256).max;
+    string public imagesFolder;
 
-    string hedgehogsMetadataURI;
+    // Whitelist
+    bytes32 public whitelistMerkleRoot;
+    uint256 public kingPriceWhitelist = 0.25 ether;
+    uint256 public whitelistActiveTime = type(uint256).max;
 
-    // these lines are called only once when the contract is deployed
+    // Per Wallet Limit
+    uint256 public maxKingsPerWallet = 2;
+
+    // Auto Approve Marketplaces
+    mapping(address => bool) public approvedProxy;
+
     constructor() {
+        _setDefaultRoyalty(msg.sender, 5_00); // 5.00%
         autoApproveMarketplace(0x1E0049783F008A0085193E00003D00cd54003c71); // OpenSea
-        autoApproveMarketplace(0xDef1C0ded9bec7F1a1670819833240f027b25EfF); // Coinbase
-        autoApproveMarketplace(0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e); // LooksRare
-        autoApproveMarketplace(0x4feE7B061C97C9c496b01DbcE9CDb10c02f0a0Be); // Rarible
-        autoApproveMarketplace(0xF849de01B080aDC3A814FaBE1E2087475cF2E354); // X2y2
     }
 
-    // Airdrop Hedgehogs
-    function giftHedgehogs(
-        address[] calldata _sendNftsTo,
-        uint256 _hedgehogsQty
-    )
-        external
-        onlyOwner
-        hedgehogsAvailable(_sendNftsTo.length * _hedgehogsQty)
-    {
-        reservedHedgehogs -= _sendNftsTo.length * _hedgehogsQty;
-        for (uint256 i = 0; i < _sendNftsTo.length; i++)
-            _safeMint(_sendNftsTo[i], _hedgehogsQty);
+    /// @notice Purchase multiple NFTs at once
+    function purchaseKings(uint256 _qty) external payable nonReentrant {
+        _safeMint(msg.sender, _qty);
+
+        require(totalSupply() <= maxSupply, "Try mint less");
+        require(tx.origin == msg.sender, "The caller is a contract");
+        require(block.timestamp > saleActiveTime, "Sale is not active");
+        require(msg.value == _qty * kingPrice, "Try to send exact amount of ETH");
+        require(_numberMinted(msg.sender) <= maxKingsPerWallet, "max kings per wallet reached");
     }
 
-    // buy / mint Hedgehogs Nfts here
-    function buyHedgehogs(uint256 _hedgehogsQty)
-        external
-        payable
-        saleActive(saleActiveTime)
-        callerIsUser
-        mintLimit(_hedgehogsQty, maxHedgehogsPerWallet)
-        priceAvailableFirstNftFree(_hedgehogsQty)
-        hedgehogsAvailable(_hedgehogsQty)
-    {
-        require(
-            _totalMinted() >= freeHedgehogs,
-            "Get your MoonHedgehogs for free"
-        );
-
-        _mint(msg.sender, _hedgehogsQty);
-    }
-
-    function buyHedgehogsFree(uint256 _hedgehogsQty)
-        external
-        saleActive(freeSaleActiveTime)
-        callerIsUser
-        mintLimit(_hedgehogsQty, freeMaxHedgehogsPerWallet)
-        hedgehogsAvailable(_hedgehogsQty)
-    {
-        require(
-            _totalMinted() < freeHedgehogs,
-            "MoonHedgehogs max free limit reached"
-        );
-
-        _mint(msg.sender, _hedgehogsQty);
-    }
-
-    // withdraw eth
+    /// @notice Owner can withdraw from here
     function withdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    // setters
-    function setHedgehogsPrice(uint256 _newPrice) external onlyOwner {
-        hedgehogsPrice = _newPrice;
+    /// @notice Change price in case of ETH price changes too much
+    function setKingPrice(uint256 _newKingPrice) external onlyOwner {
+        kingPrice = _newKingPrice;
     }
 
-    function setFreeHedgehogs(uint256 _freeHedgehogs) external onlyOwner {
-        freeHedgehogs = _freeHedgehogs;
+    function setMaxKingsPerWallet(uint256 _maxKingsPerWallet) external onlyOwner {
+        maxKingsPerWallet = _maxKingsPerWallet;
     }
 
-    function setFirstFreeMints(uint256 _firstFreeMints) external onlyOwner {
-        firstFreeMints = _firstFreeMints;
-    }
-
-    function setReservedHedgehogs(uint256 _reservedHedgehogs)
-        external
-        onlyOwner
-    {
-        reservedHedgehogs = _reservedHedgehogs;
-    }
-
-    function setMaxHedgehogsPerWallet(
-        uint256 _maxHedgehogsPerWallet,
-        uint256 _freeMaxHedgehogsPerWallet
-    ) external onlyOwner {
-        maxHedgehogsPerWallet = _maxHedgehogsPerWallet;
-        freeMaxHedgehogsPerWallet = _freeMaxHedgehogsPerWallet;
-    }
-
-    function setSaleActiveTime(
-        uint256 _saleActiveTime,
-        uint256 _freeSaleActiveTime
-    ) external onlyOwner {
+    /// @notice set sale active time
+    function setSaleActiveTime(uint256 _saleActiveTime) external onlyOwner {
         saleActiveTime = _saleActiveTime;
-        freeSaleActiveTime = _freeSaleActiveTime;
     }
 
-    function setHedgehogsMetadataURI(string memory _hedgehogsMetadataURI)
-        external
-        onlyOwner
-    {
-        hedgehogsMetadataURI = _hedgehogsMetadataURI;
+    /// @notice Hide identity or show identity from here, put images folder here, ipfs folder cid
+    function setImagesFolder(string memory __imagesFolder) external onlyOwner {
+        imagesFolder = __imagesFolder;
     }
 
-    function setRoyalty(address _receiver, uint96 _feeNumerator)
-        public
-        onlyOwner
-    {
-        _setDefaultRoyalty(_receiver, _feeNumerator);
+    /// @notice Send NFTs to a list of addresses
+    function giftNft(address[] calldata _sendNftsTo, uint256 _qty) external onlyOwner {
+        for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], _qty);
+        require(totalSupply() <= maxSupply, "Try minting less");
     }
 
-    // System Related
+    ////////////////////
+    // SYSTEM METHODS //
+    ////////////////////
+
     function _baseURI() internal view override returns (string memory) {
-        return hedgehogsMetadataURI;
+        return imagesFolder;
     }
 
     function _startTokenId() internal pure override returns (uint256) {
         return 1;
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC721A, IERC165, ERC2981)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, IERC165, ERC2981) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    // Helper Modifiers
-    modifier callerIsUser() {
-        require(tx.origin == msg.sender, "The caller is a sm");
-        _;
+    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator) external onlyOwner {
+        _setDefaultRoyalty(_receiver, _feeNumerator);
     }
 
-    modifier saleActive(uint256 _saleActiveTime) {
-        require(block.timestamp > _saleActiveTime, "Nope, sale is not open");
-        _;
+    receive() external payable {}
+
+    function receiveCoin() external payable {}
+
+    ///////////////////////////////
+    // AUTO APPROVE MARKETPLACES //
+    ///////////////////////////////
+
+    function autoApproveMarketplace(address _marketplace) public onlyOwner {
+        approvedProxy[_marketplace] = !approvedProxy[_marketplace];
     }
 
-    modifier mintLimit(uint256 _hedgehogsQty, uint256 _maxHedgehogsPerWallet) {
-        require(
-            _numberMinted(msg.sender) + _hedgehogsQty <= _maxHedgehogsPerWallet,
-            "MoonHedgehogs max x wallet exceeded"
-        );
-        _;
+    function isApprovedForAll(address _owner, address _operator) public view override(ERC721A, IERC721) returns (bool) {
+        return approvedProxy[_operator] ? true : super.isApprovedForAll(_owner, _operator);
     }
 
-    modifier hedgehogsAvailable(uint256 _hedgehogsQty) {
-        require(
-            _hedgehogsQty + totalSupply() + reservedHedgehogs <= maxSupply,
-            "Currently are sold out"
-        );
-        _;
+    ////////////////
+    // Whitelist  //
+    ////////////////
+
+    function purchaseKingsWhitelist(uint256 _qty, bytes32[] calldata _proof) external payable nonReentrant {
+        _safeMint(msg.sender, _qty);
+
+        require(totalSupply() <= maxSupply, "Try mint less");
+        require(tx.origin == msg.sender, "The caller is a contract");
+        require(inWhitelist(msg.sender, _proof), "You are not in whitelist");
+        require(block.timestamp > whitelistActiveTime, "Whitelist is not active");
+        require(msg.value == _qty * kingPriceWhitelist, "Try to send exact amount of ETH");
+        require(_numberMinted(msg.sender) <= maxKingsPerWallet, "max kings per wallet reached");
     }
 
-    modifier priceAvailable(uint256 _hedgehogsQty) {
-        require(
-            msg.value == _hedgehogsQty * hedgehogsPrice,
-            "Hey hey, send the right amount of ETH"
-        );
-        _;
+    function inWhitelist(address _owner, bytes32[] memory _proof) public view returns (bool) {
+        return MerkleProof.verify(_proof, whitelistMerkleRoot, keccak256(abi.encodePacked(_owner)));
     }
 
-    function getPrice(uint256 _qty) public view returns (uint256 price) {
-        uint256 minted = _numberMinted(msg.sender) + _qty;
-        if (minted > firstFreeMints)
-            price = (minted - firstFreeMints) * hedgehogsPrice;
+    function setWhitelistActiveTime(uint256 _whitelistActiveTime) external onlyOwner {
+        whitelistActiveTime = _whitelistActiveTime;
     }
 
-    modifier priceAvailableFirstNftFree(uint256 _hedgehogsQty) {
-        require(
-            msg.value == getPrice(_hedgehogsQty),
-            "Hey hey, send the right amount of ETH"
-        );
-        _;
+    function setWhitelistKingPrice(uint256 _kingPriceWhitelist) external onlyOwner {
+        kingPriceWhitelist = _kingPriceWhitelist;
     }
 
-    // Hedgehogs Auto Approves Marketplaces
-    mapping(address => bool) private allowed;
-
-    function autoApproveMarketplace(address _spender) public onlyOwner {
-        allowed[_spender] = !allowed[_spender];
-    }
-
-    function isApprovedForAll(address _owner, address _operator)
-        public
-        view
-        override(ERC721A, IERC721)
-        returns (bool)
-    {
-        if (
-            _operator ==
-            OpenSea(0xa5409ec958C83C3f309868babACA7c86DCB077c1).proxies(_owner)
-        ) return true;
-        else if (allowed[_operator]) return true; // Opensea or any other Marketplace
-        return super.isApprovedForAll(_owner, _operator);
+    function setWhitelist(bytes32 _whitelistMerkleRoot) external onlyOwner {
+        whitelistMerkleRoot = _whitelistMerkleRoot;
     }
 }
-
-contract MoonHedgehogsStaking is MoonHedgehogsSale {
-    mapping(address => bool) public canStake;
-
-    function addToWhitelistForStaking(address _operator) external onlyOwner {
-        canStake[_operator] = !canStake[_operator];
-    }
-
-    modifier onlyWhitelistedForStaking() {
-        require(canStake[msg.sender], "This contract is not allowed to stake");
-        _;
-    }
-
-    mapping(uint256 => bool) public staked;
-
-    function _beforeTokenTransfers(
-        address,
-        address,
-        uint256 startTokenId,
-        uint256
-    ) internal view override {
-        require(
-            !staked[startTokenId],
-            "Nope, unstake your MoonHedgehogs first"
-        );
-    }
-
-    function stakeHedgehogs(uint256[] calldata _tokenIds, bool _stake)
-        external
-        onlyWhitelistedForStaking
-    {
-        for (uint256 i = 0; i < _tokenIds.length; i++)
-            staked[_tokenIds[i]] = _stake;
-    }
-}
-
-interface OpenSea {
-    function proxies(address) external view returns (address);
-}
-
-contract MoonHedgehogs is MoonHedgehogsStaking {}
