763d2a04b1480f5d06dbc0ce315e3f4af7b1ce5f
Muneeb <muneeb.softblock@gmail.com>
2022-10-28 01:03:04 +0500

    morph name

--- contracts/Morph.sol
+++ contracts/Morph.sol
@@ -1,5 +1,5 @@
-// Crypto Kings Club
-// 10,000 Kings are Invading the Metaverse to takeover their throne as the rightful rulers
+// Crypto Morphs Club
+// 10,000 Morphs are Invading the Metaverse to takeover their throne as the rightful rulers
 
 // SPDX-License-Identifier: MIT
 
@@ -14,20 +14,27 @@ import "erc721a@3.3.0/contracts/ERC721A.sol";
 import "erc721a@3.3.0/contracts/extensions/ERC721ABurnable.sol";
 import "erc721a@3.3.0/contracts/extensions/ERC721AQueryable.sol";
 
-contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryable, ERC721ABurnable, ERC2981, Ownable, ReentrancyGuard {
-    // Main Sale
-    uint256 public kingPrice = 0.35 ether;
-    uint256 public constant maxSupply = 10000;
+contract Morph is
+    ERC721A("Morph", "MORPH"),
+    ERC721AQueryable,
+    ERC721ABurnable,
+    ERC2981,
+    Ownable,
+    ReentrancyGuard
+{
+    // Whitelist Config
+    bytes32 public whitelistMerkleRoot;
+    uint256 public morphPriceWhitelist = 0 ether;
+    uint256 public whitelistActiveTime = type(uint256).max;
+
+    // Main Sale Config
+    uint256 public morphPrice = 0.5 ether;
+    uint256 public constant maxSupply = 1000;
     uint256 public saleActiveTime = type(uint256).max;
     string public imagesFolder;
 
-    // Whitelist
-    bytes32 public whitelistMerkleRoot;
-    uint256 public kingPriceWhitelist = 0.25 ether;
-    uint256 public whitelistActiveTime = type(uint256).max;
-
     // Per Wallet Limit
-    uint256 public maxKingsPerWallet = 2;
+    uint256 public maxMorphsPerWallet = 2;
 
     // Auto Approve Marketplaces
     mapping(address => bool) public approvedProxy;
@@ -37,29 +44,38 @@ contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryabl
         autoApproveMarketplace(0x1E0049783F008A0085193E00003D00cd54003c71); // OpenSea
     }
 
-    /// @notice Purchase multiple NFTs at once
-    function purchaseKings(uint256 _qty) external payable nonReentrant {
+    /// @notice Purchase NFTs
+    function purchaseMorphs(uint256 _qty) external payable nonReentrant {
         _safeMint(msg.sender, _qty);
 
         require(totalSupply() <= maxSupply, "Try mint less");
         require(tx.origin == msg.sender, "The caller is a contract");
         require(block.timestamp > saleActiveTime, "Sale is not active");
-        require(msg.value == _qty * kingPrice, "Try to send exact amount of ETH");
-        require(_numberMinted(msg.sender) <= maxKingsPerWallet, "max kings per wallet reached");
+        require(
+            msg.value == _qty * morphPrice,
+            "Try to send exact amount of ETH"
+        );
+        require(
+            _numberMinted(msg.sender) <= maxMorphsPerWallet,
+            "Max morphs per wallet reached"
+        );
     }
 
-    /// @notice Owner can withdraw from here
+    /// @notice Owner can withdraw ETH from here
     function withdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
     /// @notice Change price in case of ETH price changes too much
-    function setKingPrice(uint256 _newKingPrice) external onlyOwner {
-        kingPrice = _newKingPrice;
+    function setMorphPrice(uint256 _newMorphPrice) external onlyOwner {
+        morphPrice = _newMorphPrice;
     }
 
-    function setMaxKingsPerWallet(uint256 _maxKingsPerWallet) external onlyOwner {
-        maxKingsPerWallet = _maxKingsPerWallet;
+    function setMaxMorphsPerWallet(uint256 _maxMorphsPerWallet)
+        external
+        onlyOwner
+    {
+        maxMorphsPerWallet = _maxMorphsPerWallet;
     }
 
     /// @notice set sale active time
@@ -73,14 +89,18 @@ contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryabl
     }
 
     /// @notice Send NFTs to a list of addresses
-    function giftNft(address[] calldata _sendNftsTo, uint256 _qty) external onlyOwner {
-        for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], _qty);
+    function giftNft(address[] calldata _sendNftsTo, uint256 _qty)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _sendNftsTo.length; i++)
+            _safeMint(_sendNftsTo[i], _qty);
         require(totalSupply() <= maxSupply, "Try minting less");
     }
 
-    ////////////////////
-    // SYSTEM METHODS //
-    ////////////////////
+    //////////////////////
+    // STANDARD METHODS //
+    //////////////////////
 
     function _baseURI() internal view override returns (string memory) {
         return imagesFolder;
@@ -90,11 +110,20 @@ contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryabl
         return 1;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, IERC165, ERC2981) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721A, IERC165, ERC2981)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
-    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator) external onlyOwner {
+    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator)
+        external
+        onlyOwner
+    {
         _setDefaultRoyalty(_receiver, _feeNumerator);
     }
 
@@ -110,35 +139,71 @@ contract CryptoKingsClub is ERC721A("Crypto Kings Club", "CKC"), ERC721AQueryabl
         approvedProxy[_marketplace] = !approvedProxy[_marketplace];
     }
 
-    function isApprovedForAll(address _owner, address _operator) public view override(ERC721A, IERC721) returns (bool) {
-        return approvedProxy[_operator] ? true : super.isApprovedForAll(_owner, _operator);
+    function isApprovedForAll(address _owner, address _operator)
+        public
+        view
+        override(ERC721A, IERC721)
+        returns (bool)
+    {
+        return
+            approvedProxy[_operator]
+                ? true
+                : super.isApprovedForAll(_owner, _operator);
     }
 
     ////////////////
     // Whitelist  //
     ////////////////
 
-    function purchaseKingsWhitelist(uint256 _qty, bytes32[] calldata _proof) external payable nonReentrant {
+    function purchaseMorphsWhitelist(uint256 _qty, bytes32[] calldata _proof)
+        external
+        payable
+        nonReentrant
+    {
         _safeMint(msg.sender, _qty);
 
         require(totalSupply() <= maxSupply, "Try mint less");
         require(tx.origin == msg.sender, "The caller is a contract");
         require(inWhitelist(msg.sender, _proof), "You are not in whitelist");
-        require(block.timestamp > whitelistActiveTime, "Whitelist is not active");
-        require(msg.value == _qty * kingPriceWhitelist, "Try to send exact amount of ETH");
-        require(_numberMinted(msg.sender) <= maxKingsPerWallet, "max kings per wallet reached");
+        require(
+            block.timestamp > whitelistActiveTime,
+            "Whitelist is not active"
+        );
+        require(
+            msg.value == _qty * morphPriceWhitelist,
+            "Try to send exact amount of ETH"
+        );
+        require(
+            _numberMinted(msg.sender) == 1,
+            "Only 1 spot is available in whitelist"
+        );
     }
 
-    function inWhitelist(address _owner, bytes32[] memory _proof) public view returns (bool) {
-        return MerkleProof.verify(_proof, whitelistMerkleRoot, keccak256(abi.encodePacked(_owner)));
+    function inWhitelist(address _owner, bytes32[] memory _proof)
+        public
+        view
+        returns (bool)
+    {
+        return
+            MerkleProof.verify(
+                _proof,
+                whitelistMerkleRoot,
+                keccak256(abi.encodePacked(_owner))
+            );
     }
 
-    function setWhitelistActiveTime(uint256 _whitelistActiveTime) external onlyOwner {
+    function setWhitelistActiveTime(uint256 _whitelistActiveTime)
+        external
+        onlyOwner
+    {
         whitelistActiveTime = _whitelistActiveTime;
     }
 
-    function setWhitelistKingPrice(uint256 _kingPriceWhitelist) external onlyOwner {
-        kingPriceWhitelist = _kingPriceWhitelist;
+    function setWhitelistMorphPrice(uint256 _morphPriceWhitelist)
+        external
+        onlyOwner
+    {
+        morphPriceWhitelist = _morphPriceWhitelist;
     }
 
     function setWhitelist(bytes32 _whitelistMerkleRoot) external onlyOwner {
