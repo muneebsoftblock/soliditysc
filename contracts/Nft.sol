60c7ce8ecf4aa2ff80c8c3f24bc496c9ab506dca
Muneeb <muneeb.softblock@gmail.com>
2022-11-29 01:38:05 +0500

    compiling in remix

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -2,9 +2,8 @@
 
 pragma solidity ^0.8.17;
 
-
 import "erc721a@4.2.3/contracts/ERC721A.sol";
-import {DefaultOperatorFilterer} from "https://github.com/ProjectOpenSea/operator-filter-registry/blob/main/src/DefaultOperatorFilterer.sol";    
+import {DefaultOperatorFilterer} from "https://github.com/ProjectOpenSea/operator-filter-registry/blob/main/src/DefaultOperatorFilterer.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
@@ -13,7 +12,12 @@ interface OpenSea {
     function proxies(address) external view returns (address);
 }
 
-contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFilterer {
+contract Sample is
+    ERC721A("Sample", "NTO"),
+    Ownable,
+    ERC2981,
+    DefaultOperatorFilterer
+{
     bool public revealed = false;
     string public notRevealedMetadataFolderIpfsLink;
     uint256 public maxMintAmount = 10;
@@ -31,12 +35,21 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
 
     // public
     function purchaseTokens(uint256 _mintAmount) public payable {
-        require(block.timestamp > publicmintActiveTime, "the contract is paused");
+        require(
+            block.timestamp > publicmintActiveTime,
+            "the contract is paused"
+        );
         uint256 supply = totalSupply();
         require(_mintAmount > 0, "need to mint at least 1 NFT");
-        require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-        require(supply + _mintAmount + nftsForOwner <= maxSupply, "max NFT limit exceeded");
-        require(msg.value >= costPerNft * _mintAmount, "insufficient funds");
+        require(
+            _numberMinted(msg.sender) + _mintAmount <= maxMintAmount,
+            "max mint amount per session exceeded"
+        );
+        require(
+            supply + _mintAmount + nftsForOwner <= maxSupply,
+            "max NFT limit exceeded"
+        );
+        require(msg.value == costPerNft * _mintAmount, "insufficient funds");
 
         _safeMint(msg.sender, _mintAmount);
     }
@@ -45,7 +58,13 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
     //       OVERRIDE CODE STARTS    //
     ///////////////////////////////////
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, ERC2981) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721A, ERC2981)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
@@ -57,13 +76,31 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
         return metadataFolderIpfsLink;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override(ERC721A) returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721A)
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
 
         if (revealed == false) return notRevealedMetadataFolderIpfsLink;
 
         string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, _toString(tokenId), baseExtension)) : "";
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        _toString(tokenId),
+                        baseExtension
+                    )
+                )
+                : "";
     }
 
     //////////////////
@@ -71,21 +108,30 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
     //////////////////
 
     function withdraw() public payable onlyOwner {
-        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(success);
     }
 
-    function giftNft(address[] calldata _sendNftsTo, uint256 _howMany) external onlyOwner {
+    function giftNft(address[] calldata _sendNftsTo, uint256 _howMany)
+        external
+        onlyOwner
+    {
         nftsForOwner -= _sendNftsTo.length * _howMany;
 
-        for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], _howMany);
+        for (uint256 i = 0; i < _sendNftsTo.length; i++)
+            _safeMint(_sendNftsTo[i], _howMany);
     }
 
     function setnftsForOwner(uint256 _newnftsForOwner) public onlyOwner {
         nftsForOwner = _newnftsForOwner;
     }
 
-    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator) public onlyOwner {
+    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator)
+        public
+        onlyOwner
+    {
         _setDefaultRoyalty(_receiver, _feeNumerator);
     }
 
@@ -101,11 +147,16 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
         maxMintAmount = _newmaxMintAmount;
     }
 
-    function setMetadataFolderIpfsLink(string memory _newMetadataFolderIpfsLink) public onlyOwner {
+    function setMetadataFolderIpfsLink(string memory _newMetadataFolderIpfsLink)
+        public
+        onlyOwner
+    {
         metadataFolderIpfsLink = _newMetadataFolderIpfsLink;
     }
 
-    function setNotRevealedMetadataFolderIpfsLink(string memory _notRevealedMetadataFolderIpfsLink) public onlyOwner {
+    function setNotRevealedMetadataFolderIpfsLink(
+        string memory _notRevealedMetadataFolderIpfsLink
+    ) public onlyOwner {
         notRevealedMetadataFolderIpfsLink = _notRevealedMetadataFolderIpfsLink;
     }
 
@@ -115,50 +166,63 @@ contract Sample is ERC721A("Sample", "NTO"), Ownable, ERC2981, DefaultOperatorFi
 }
 
 contract NftWhitelistSaleMerkle is Sample {
-    ///////////////////////////////
-    //    PRESALE CODE STARTS    //
-    ///////////////////////////////
+    // multiple presale configs
+    mapping(uint256 => uint256) public maxMintPresales;
+    mapping(uint256 => uint256) public itemPricePresales;
+    mapping(uint256 => bytes32) public whitelistMerkleRoots;
+    uint256 public presaleActiveTime = type(uint256).max;
 
-    uint256 public presaleActiveTime = 0;
-    uint256 public presaleMaxMint = 1;
-    bytes32 public whitelistMerkleRoot;
-    uint256 public itemPricePresale = 0.2 * 1e18;
-    mapping(address => uint256) public presaleClaimedBy;
-
-    function setWhitelist(bytes32 _whitelistMerkleRoot) external onlyOwner {
-        whitelistMerkleRoot = _whitelistMerkleRoot;
+    function _inWhitelist(
+        address _owner,
+        bytes32[] memory _proof,
+        uint256 _rootNumber
+    ) private view returns (bool) {
+        return
+            MerkleProof.verify(
+                _proof,
+                whitelistMerkleRoots[_rootNumber],
+                keccak256(abi.encodePacked(_owner))
+            );
     }
 
-    function inWhitelist(bytes32[] memory _proof, address _owner) public view returns (bool) {
-        return MerkleProof.verify(_proof, whitelistMerkleRoot, keccak256(abi.encodePacked(_owner)));
-    }
-
-    function purchaseTokensPresale(uint256 _howMany, bytes32[] calldata _proof) external payable {
-        uint256 supply = totalSupply();
-        require(supply + _howMany + nftsForOwner <= maxSupply, "max NFT limit exceeded");
-
-        require(inWhitelist(_proof, msg.sender), "You are not in presale");
+    function purchaseTokensWhitelist(
+        uint256 _howMany,
+        bytes32[] calldata _proof,
+        uint256 _rootNumber
+    ) external payable {
         require(block.timestamp > presaleActiveTime, "Presale is not active");
-        require(msg.value >= _howMany * itemPricePresale, "Try to send more ETH");
-
-        presaleClaimedBy[msg.sender] += _howMany;
-
-        require(presaleClaimedBy[msg.sender] <= presaleMaxMint, "Purchase exceeds max allowed");
+        require(
+            _inWhitelist(msg.sender, _proof, _rootNumber),
+            "You are not in presale"
+        );
+        require(
+            msg.value == _howMany * itemPricePresales[_rootNumber],
+            "Try to send more ETH"
+        );
+        require(
+            _numberMinted(msg.sender) + _howMany <=
+                maxMintPresales[_rootNumber],
+            "Purchase exceeds max allowed"
+        );
 
         _safeMint(msg.sender, _howMany);
     }
 
-    // set limit of presale
-    function setPresaleMaxMint(uint256 _presaleMaxMint) external onlyOwner {
-        presaleMaxMint = _presaleMaxMint;
+    function setPresale(
+        uint256 _rootNumber,
+        bytes32 _whitelistMerkleRoot,
+        uint256 _maxMintPresales,
+        uint256 _itemPricePresale
+    ) external onlyOwner {
+        maxMintPresales[_rootNumber] = _maxMintPresales;
+        itemPricePresales[_rootNumber] = _itemPricePresale;
+        whitelistMerkleRoots[_rootNumber] = _whitelistMerkleRoot;
     }
 
-    // Change presale price in case of ETH price changes too much
-    function setPricePresale(uint256 _itemPricePresale) external onlyOwner {
-        itemPricePresale = _itemPricePresale;
-    }
-
-    function setPresaleActiveTime(uint256 _presaleActiveTime) external onlyOwner {
+    function setPresaleActiveTime(uint256 _presaleActiveTime)
+        external
+        onlyOwner
+    {
         presaleActiveTime = _presaleActiveTime;
     }
 
