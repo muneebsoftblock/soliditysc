18b5befb1feac1114cd55196bd85d1aa0c976bad
Ahmed Raza <ahmedrazach118@gmail.com>
2023-01-12 18:52:13 +0500

    improved variable, function names, comments added

--- contracts/Dutch.sol
+++ contracts/Dutch.sol
@@ -2,28 +2,44 @@
 
 pragma solidity ^0.8.17;
 
+// imported smart contracts from openzepplin 
 import "erc721a@4.2.3/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {DefaultOperatorFilterer} from "https://github.com/ProjectOpenSea/operator-filter-registry/blob/main/src/DefaultOperatorFilterer.sol";
 
+// NftPublicSale smart contract is inherited from ERC721a, Ownable, ERC2981 and implemented DefaultOperatorFilter from OpenSea
 contract NftPublicSale is
     ERC721A("TechnoFeudal", "TF"),
     Ownable,
     ERC2981,
     DefaultOperatorFilterer
 {
-    bool public revealed = false;
-    uint256 public maxSupply = 10_000;
-    uint256 public maxMintAmount = 20;
+    // nftRevealed is boolean type variable that tells us that NFTs are revealed for the auction or not.
+    bool public nftRevealed = false;
+    // maximum supply of NFTs variable holdes the limit of NFTs supplied
+    uint256 public maxNFTSupply = 10_000;
+    // NFT minting fee will be maximum 20
+    uint256 public maxNFTMintAmout = 20;
+    // Owner owns the NFTs 
     uint256 public nftsForOwner = 250;
-    uint256 public maxMintForActiveSale;
+    // Maximum NFTs which is minted for Active sale
+    uint256 public maxNftsMintForSale;
+    // this variable holdes the link of IPFS for NFTs metadata
     string public metadataFolderIpfsLink;
+
     uint256 public nftPerAddressLimit = 3;
     string constant baseExtension = ".json";
+
     uint256 public costPerNft = 0.075 * 1e18;
+
     string public notRevealedMetadataFolderIpfsLink;
+    // this variable holdes the active time for nft minting this will be 365 days onward from the first transaction occured
+
     uint256 public publicMintActiveTime = block.timestamp + 365 days; // gte value from https://www.epochconverter.com/
+    // smart contract will runs once when the smart contract would be deployed 
+    // this will set the royality for the ower on which address the smartcontract is deployed
+    // this would be the 10% of the transaction
 
     constructor() {
         _setDefaultRoyalty(msg.sender, 10_00); // 10.00 %
@@ -31,29 +47,37 @@ contract NftPublicSale is
 
     // Public Sale function
     function purchaseTokens(uint256 _mintAmount) public payable {
+        // this will check if the minting time is allowed and the contract is active 
         require(
             block.timestamp > publicMintActiveTime,
             "the contract is paused"
         );
+        // supply variable will holdes the value of the total supply of NFTs
         uint256 supply = totalSupply();
+        // this will check if there is NFTs available for acution or minted. if not this will restrict the transaction
         require(_mintAmount > 0, "need to mint at least 1 NFT");
+        // this will check the session minting amount limit. If the minting amount limit exceeds the allowd limit this will restrict the transaction
         require(
-            _numberMinted(msg.sender) + _mintAmount <= maxMintAmount,
+            _numberMinted(msg.sender) + _mintAmount <= maxNFTMintAmout,
             "max mint amount per session exceeded"
         );
+        // this will check if there is NFT limit is available 
         require(
-            supply + _mintAmount + nftsForOwner <= maxSupply,
+            supply + _mintAmount + nftsForOwner <= maxNFTSupply,
             "max NFT limit exceeded"
         );
+        // this will check if the minting limit that is applied on NFT minting is approached or not
         require(
-            supply + _mintAmount + nftsForOwner <= maxMintForActiveSale,
+            supply + _mintAmount + nftsForOwner <= maxNftsMintForSale,
             "Max Mint limit exceeded of this sale."
         );
+        // this will check if there is enough balance available to execute this transaction
         require(msg.value >= costPerNft * _mintAmount, "insufficient funds");
-
+        // if all the requrements satisfied safemint function will executes
         _safeMint(msg.sender, _mintAmount);
     }
 
+    // this function will override the functionalites of the interfaces ERC721a and ERC2981 and will return the if the interfaces supports or not in boolean
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -63,18 +87,21 @@ contract NftPublicSale is
     {
         return super.supportsInterface(interfaceId);
     }
-
+    // helper function to start the token 
     // Token ID start from 1
     function _startTokenId() internal pure override returns (uint256) {
         return 1;
     }
-
+    // helper function to this will returns the IPFS link for the metadata used for NFTs
     // Base URI of IPFS metadata folder
     function _baseURI() internal view virtual override returns (string memory) {
         return metadataFolderIpfsLink;
     }
 
+
+    // returns the URI of the NFTs Tokens 
     // Token URI of minted tokens
+    // this function will override the functionalities of the ERC721a 
     function tokenURI(uint256 tokenId)
         public
         view
@@ -82,14 +109,16 @@ contract NftPublicSale is
         override(ERC721A)
         returns (string memory)
     {
+        // this will check if the token ID exists or not 
         require(
             _exists(tokenId),
             "ERC721Metadata: URI query for nonexistent token"
         );
-
-        if (revealed == false) return notRevealedMetadataFolderIpfsLink;
+        // this will check if the NFT which is trying to access is revealed or not if not returns not revealed
+        if (nftRevealed == false) return notRevealedMetadataFolderIpfsLink;
 
         string memory currentBaseURI = _baseURI();
+        // this will retruns the URI for the token
         return
             bytes(currentBaseURI).length > 0
                 ? string(
@@ -111,15 +140,16 @@ contract NftPublicSale is
     }
 
     // Only owner can set Minting amount for active sale
-    function setMintForActiveSale(uint256 _maxMintForActiveSale)
+    // this will sets the limit on the NFTs which can be minted for sale
+    function setMaxNFTsLimitForAuction(uint256 _maxMintForActiveSale)
         external
         onlyOwner
     {
-        maxMintForActiveSale = _maxMintForActiveSale;
+        maxNftsMintForSale = _maxMintForActiveSale;
     }
 
     // Owner can gift NFTs to multiple addresses
-    function giftNft(address[] calldata _sendNftsTo, uint256 _howMany)
+    function giftNFT(address[] calldata _sendNftsTo, uint256 _howMany)
         external
         onlyOwner
     {
@@ -130,6 +160,8 @@ contract NftPublicSale is
     }
 
     //Owner can set Default Royalty
+    // Royality works as at every time if an NFT is traded so percentage of the transaction will go to the minter who minted the NFT.
+    // in our case it will be 10% of the transaction or price of the NFT
     function setDefaultRoyalty(address _receiver, uint96 _feeNumerator)
         public
         onlyOwner
@@ -138,14 +170,17 @@ contract NftPublicSale is
     }
 
     // Owner can reveal NFTs
-    function revealFlip() public onlyOwner {
-        revealed = !revealed;
+    // if the NFT is not revealed it will reveal it or if already revealed it will reverse it
+    function revealNftFlip() public onlyOwner {
+        nftRevealed = !nftRevealed;
     }
-
+    
+    
     // Owner can set NFT per address limit
     function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
         nftPerAddressLimit = _limit;
     }
+
 
     // Owner can set Per NFT cost
     function setCostPerNft(uint256 _newCostPerNft) public onlyOwner {
@@ -154,7 +189,7 @@ contract NftPublicSale is
 
     // Owner can set max mint amount at once
     function setMaxMintAmount(uint256 _newMaxMintAmount) public onlyOwner {
-        maxMintAmount = _newMaxMintAmount;
+        maxNFTMintAmout = _newMaxMintAmount;
     }
 
     // Owner can set Revealed NFTs IPFS metadata folder base URI
@@ -260,15 +295,15 @@ contract NftDutchAuctionSale is NftPublicSale {
         uint256 supply = totalSupply();
         require(_mintAmount > 0, "need to mint at least 1 NFT");
         require(
-            _numberMinted(msg.sender) + _mintAmount <= maxMintAmount,
+            _numberMinted(msg.sender) + _mintAmount <= maxNFTMintAmout,
             "max mint amount per session exceeded"
         );
         require(
-            supply + _mintAmount + nftsForOwner <= maxSupply,
+            supply + _mintAmount + nftsForOwner <= maxNFTSupply,
             "max NFT limit exceeded"
         );
         require(
-            supply + _mintAmount + nftsForOwner <= maxMintForActiveSale,
+            supply + _mintAmount + nftsForOwner <= maxNftsMintForSale,
             "max NFT limit exceeded"
         );
         require(msg.value >= price * _mintAmount, "insufficient funds");
