95e83fa980650b57a90ee8f1c62adcd32dbf930b
Ahmed Raza <ahmedrazach118@gmail.com>
2023-02-15 22:12:25 +0500

    initial commit at branch digicollect-ahmed

--- contracts/DigiCollect.sol
+++ contracts/DigiCollect.sol
@@ -7,7 +7,7 @@
 //
 // dua, 80 20 in coding, can actually check from last year IA
 
-pragma solidity 0.8.14;
+pragma solidity ^=0.8.14;
 
 import "erc721a/contracts/ERC721A.sol";
 import "erc721a/contracts/extensions/ERC721AQueryable.sol";
