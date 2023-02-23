c159ab26373c5ae30a7ac7d43a7ccaea95e21140
Ahmed Raza <ahmedrazach118@gmail.com>
2023-02-23 20:30:33 +0500

    DIGI connecting with DigiCollect

--- migrations/2_migration.js
+++ migrations/2_migration.js
@@ -1,5 +1,15 @@
-const sc = artifacts.require('DigiCollect');
+// const sc = artifacts.require('DigiCollect');
+
+// module.exports = function (deployer) {
+//   deployer.deploy(sc);
+// };
+
+const DIGI = artifacts.require("DIGI");
+const Digicollect = artifacts.require("Digicollect");
 
 module.exports = function (deployer) {
-  deployer.deploy(sc);
+  deployer.deploy(DIGI).then(function() {
+    return deployer.deploy(Digicollect, DIGI.address);
+  });
 };
+
