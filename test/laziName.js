8429194f8b729417288e6d4141bb6edb57af5cab
Muneeb <muneeb.softblock@gmail.com>
2023-03-01 18:02:52 +0300

    test pass, gas ok

--- test/laziName.js
+++ test/laziName.js
@@ -1,50 +1,28 @@
-const MoonHedgehogs = artifacts.require('LaziName');
+const LaziName = artifacts.require('LaziName')
 
 contract('LaziName', ([alice, bob, carol, owner]) => {
   it('should assert true', async () => {
-    const sc = await MoonHedgehogs.new({ from: owner });
-
-    const scOwner = await sc.owner();
-    const scAddr = sc.address;
-    console.log({ scOwner, scAddr });
-
-    await sc.setSaleActiveTime(0, 0, { from: owner });
-    await sc.setFirstFreeMints(2, { from: owner });
-    await sc.setMaxLaziNamePerWallet(5, 0, { from: owner });
-
+    const sc = await LaziName.new({ from: owner })
+    await sc.set_saleActiveTime(0, { from: owner })
     {
-      const qty = 1;
-      const from = alice;
-      const price = '' + (await sc.getPrice(qty, { from }));
-      console.log({ price });
-      await sc.buyLaziName(qty, {
+      const names = ['muneeb.lazi', 'adil.lazi']
+      const from = alice
+      const qty = names.length;
+      const price = '' + (await sc.getPrice(qty, { from }))
+      console.log({ price })
+      await sc.buyLaziNames(names, {
         from,
         value: price,
-      });
-    }
-    {
-      const qty = 1;
-      const from = bob;
-      const price = '' + (await sc.getPrice(qty, { from }));
-      console.log({ price });
-      await sc.buyLaziName(qty, {
-        from,
-        value: price,
-      });
-    }
-    {
-      const qty = 1;
-      const from = carol;
-      const price = '' + (await sc.getPrice(qty, { from }));
-      console.log({ price });
-      await sc.buyLaziName(qty, {
-        from,
-        value: price,
-      });
+      })
     }
 
-    await sc.withdraw({ from: owner });
+    const balanceBefore = await web3.eth.getBalance(owner)
+    await sc.withdraw({ from: owner })
+    const balanceAfter = await web3.eth.getBalance(owner)
+    console.log(
+      `balanceBefore ${balanceBefore} balanceAfter ${balanceAfter} type ${typeof balanceBefore}`
+    )
 
-    return assert.isTrue(true);
-  });
-});
+    assert(balanceAfter > balanceBefore)
+  })
+})
