49be9ca3a11e04e4aea4b26b6a8afcdc966ca5a3
Muneeb <muneeb.softblock@gmail.com>
2023-04-05 16:09:31 +0500

    code optimized for reentrancy

--- contracts/LaziName.sol
+++ contracts/LaziName.sol
@@ -16,7 +16,7 @@ import "erc721a/contracts/extensions/ERC721AQueryable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-contract LaziName is
+contract Nft is
     ERC721A("Lazi Name Service", "LNS"),
     Ownable,
     ERC721AQueryable,
@@ -31,15 +31,6 @@ contract LaziName is
 
     string laziNameImages;
 
-    // these lines are called only once when the contract is deployed
-    constructor() {
-        autoApproveMarketplace(0x1E0049783F008A0085193E00003D00cd54003c71); // OpenSea
-        autoApproveMarketplace(0xDef1C0ded9bec7F1a1670819833240f027b25EfF); // Coinbase
-        autoApproveMarketplace(0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e); // LooksRare
-        autoApproveMarketplace(0x4feE7B061C97C9c496b01DbcE9CDb10c02f0a0Be); // Rarible
-        autoApproveMarketplace(0xF849de01B080aDC3A814FaBE1E2087475cF2E354); // X2y2
-    }
-
     function registerName(string calldata _laziName, uint256 tokenId) internal {
         require(!isMinted[_laziName], "Nft Domain Already Minted");
         domainNameOf[tokenId] = _laziName;
@@ -53,6 +44,8 @@ contract LaziName is
         uint256 startId = totalSupply() + _startTokenId();
         for (uint256 i = 0; i < _laziNames.length; i++) {
             registerName(_laziNames[i], startId + i);
+        }
+        for (uint256 i = 0; i < _laziNames.length; i++) {
             _safeMint(_addresses[i], 1);
         }
     }
@@ -129,21 +122,6 @@ contract LaziName is
         _;
     }
 
-    // LaziName Auto Approves Marketplaces
-    mapping(address => bool) private allowed;
-
-    function autoApproveMarketplace(address _spender) public onlyOwner {
-        allowed[_spender] = !allowed[_spender];
-    }
-
-    function isApprovedForAll(
-        address _owner,
-        address _operator
-    ) public view override(ERC721A, IERC721) returns (bool) {
-        if (allowed[_operator]) return true; // Opensea or any other Marketplace
-        return super.isApprovedForAll(_owner, _operator);
-    }
-
     // System Related
     function _baseURI() internal view override returns (string memory) {
         return laziNameImages;
@@ -159,3 +137,31 @@ contract LaziName is
         return super.supportsInterface(interfaceId);
     }
 }
+
+contract AutoApproveMarketplaces is Nft {
+    // LaziName Auto Approves Marketplaces
+    mapping(address => bool) private allowed;
+
+    // these lines are called only once when the contract is deployed
+    constructor() {
+        autoApproveMarketplace(0xF849de01B080aDC3A814FaBE1E2087475cF2E354); // X2y2
+        autoApproveMarketplace(0x4feE7B061C97C9c496b01DbcE9CDb10c02f0a0Be); // Rarible
+        autoApproveMarketplace(0x1E0049783F008A0085193E00003D00cd54003c71); // OpenSea
+        autoApproveMarketplace(0xDef1C0ded9bec7F1a1670819833240f027b25EfF); // Coinbase
+        autoApproveMarketplace(0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e); // LooksRare
+    }
+
+    function autoApproveMarketplace(address _spender) public onlyOwner {
+        allowed[_spender] = !allowed[_spender];
+    }
+
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public view override(ERC721A, IERC721) returns (bool) {
+        if (allowed[_operator]) return true; // Opensea or any other Marketplace
+        return super.isApprovedForAll(_owner, _operator);
+    }
+}
+
+contract LaziName is AutoApproveMarketplaces {}
