a37322256104d3e8c891150ed8f44cdd77c943bb
Muneeb <muneeb.softblock@gmail.com>
2023-05-02 16:34:40 +0500

    stake test, node v18.13.0

---
+++ contracts/StakeLazi2.sol
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Staking is ERC721Holder, Ownable {
+    uint256 private constant SECONDS_PER_DAY = 86400;
+    uint256 private constant TOTAL_REWARD_TOKENS = 200_000_000;
+    uint256 private constant REWARD_PERIOD = 4 * 365 * SECONDS_PER_DAY;
+
+    IERC20 public rewardToken;
+    IERC721 public nftToken;
+
+    struct StakeInfo {
+        uint256 stakedTimestamp;
+        uint256 erc20Amount;
+        uint256[] erc721Ids;
+        uint256 claimedRewards;
+    }
+
+    mapping(address => StakeInfo) public stakes;
+
+    mapping(uint256 => uint256) private lockPeriodDistribution;
+    mapping(uint256 => uint256) private stakedTokensDistribution;
+    mapping(uint256 => uint256) private rewardTokensDistribution;
+    mapping(uint256 => uint256) private apyDistribution;
+
+    constructor(IERC20 _rewardToken, IERC721 _nftToken) {
+        rewardToken = _rewardToken;
+        nftToken = _nftToken;
+    }
+
+    function getRewardMultiplier(uint256 stakingDays, uint256 erc721Tokens) public pure returns (uint256) {
+        uint256 timeMultiplier;
+
+        if (stakingDays < 90) {
+            timeMultiplier = 100;
+        } else if (stakingDays < 180) {
+            timeMultiplier = 125;
+        } else if (stakingDays < 365) {
+            timeMultiplier = 150;
+        } else if (stakingDays < 545) {
+            timeMultiplier = 200;
+        } else if (stakingDays < 730) {
+            timeMultiplier = 275;
+        } else {
+            timeMultiplier = 350;
+        }
+
+        uint256 erc721Multiplier;
+
+        if (erc721Tokens == 0) {
+            erc721Multiplier = 100;
+        } else if (erc721Tokens == 1) {
+            erc721Multiplier = 120;
+        } else if (erc721Tokens == 2) {
+            erc721Multiplier = 140;
+        } else if (erc721Tokens == 3) {
+            erc721Multiplier = 160;
+        } else if (erc721Tokens == 4) {
+            erc721Multiplier = 180;
+        } else {
+            erc721Multiplier = 200;
+        }
+
+        return (timeMultiplier * erc721Multiplier) / 100;
+    }
+
+    function stake(uint256 erc20Amount, uint256 daysToStake, uint256[] calldata erc721Ids) external {
+        require(erc20Amount > 0, "Must stake a positive amount");
+        require(daysToStake > 0, "Must stake for a positive number of days");
+        require(stakes[msg.sender].erc20Amount == 0, "Must unstake before staking again");
+
+        uint256 erc721Amount = erc721Ids.length;
+        for (uint256 i = 0; i < erc721Amount; i++) {
+            nftToken.safeTransferFrom(msg.sender, address(this), erc721Ids[i]);
+        }
+
+        rewardToken.transferFrom(msg.sender, address(this), erc20Amount);
+
+        stakes[msg.sender] = StakeInfo({stakedTimestamp: block.timestamp, erc20Amount: erc20Amount, erc721Ids: erc721Ids, claimedRewards: 0});
+
+        updateDistributions(daysToStake, erc20Amount, 0);
+    }
+
+    function unstake() external {
+        StakeInfo storage stakeInfo = stakes[msg.sender];
+        require(stakeInfo.erc20Amount > 0, "No tokens staked");
+
+        rewardToken.transfer(msg.sender, stakeInfo.erc20Amount);
+
+        for (uint256 i = 0; i < stakeInfo.erc721Ids.length; i++) {
+            nftToken.safeTransferFrom(address(this), msg.sender, stakeInfo.erc721Ids[i]);
+        }
+
+        delete stakes[msg.sender];
+    }
+
+    function harvestRewards() external {
+        uint256 rewards = getUserRewards(msg.sender);
+        require(rewards > 0, "No rewards available");
+
+        rewardToken.transfer(msg.sender, rewards);
+        stakes[msg.sender].claimedRewards += rewards;
+
+        updateDistributions(0, 0, rewards);
+    }
+
+    function compoundRewards() external {
+        uint256 rewards = getUserRewards(msg.sender);
+        require(rewards > 0, "No rewards available");
+
+        stakes[msg.sender].erc20Amount += rewards;
+        stakes[msg.sender].claimedRewards += rewards;
+    }
+
+    function getUserRewards(address user) public view returns (uint256) {
+        StakeInfo storage stakeInfo = stakes[user];
+
+        if (stakeInfo.erc20Amount == 0) {
+            return 0;
+        }
+
+        uint256 stakingDays = (block.timestamp - stakeInfo.stakedTimestamp) / SECONDS_PER_DAY;
+        uint256 rewardMultiplier = getRewardMultiplier(stakingDays, stakeInfo.erc721Ids.length);
+
+        uint256 totalRewards = (stakeInfo.erc20Amount * REWARD_PERIOD * rewardMultiplier) / (TOTAL_REWARD_TOKENS * 10000);
+        uint256 pendingRewards = totalRewards - stakeInfo.claimedRewards;
+
+        return pendingRewards;
+    }
+
+    function updateDistributions(uint256 daysToStake, uint256 erc20Amount, uint256 rewards) private {
+        lockPeriodDistribution[daysToStake] += 1;
+        stakedTokensDistribution[daysToStake] += erc20Amount;
+        rewardTokensDistribution[daysToStake] += rewards;
+        apyDistribution[daysToStake] = getCurrentAPY();
+    }
+
+    function getLockPeriodDistribution(uint256 daysToStake) public view returns (uint256) {
+        return lockPeriodDistribution[daysToStake];
+    }
+
+    function getStakedTokensDistribution(uint256 daysToStake) public view returns (uint256) {
+        return stakedTokensDistribution[daysToStake];
+    }
+
+    function getRewardTokensDistribution(uint256 daysToStake) public view returns (uint256) {
+        return rewardTokensDistribution[daysToStake];
+    }
+
+    function getAPYDistribution(uint256 daysToStake) public view returns (uint256) {
+        return apyDistribution[daysToStake];
+    }
+
+    // function getCurrentAPR() public view returns (uint256) {
+    //     uint256 totalStaked = rewardToken.balanceOf(address(this));
+    //     uint256 totalRewardsPerYear = (TOTAL_REWARD_TOKENS * SECONDS_PER_DAY) / REWARD_PERIOD;
+    //     return (totalRewardsPerYear * 100) / totalStaked;
+    // }
+
+    function getCurrentAPR() public view returns (uint256) {
+        uint256 totalStaked = rewardToken.balanceOf(address(this));
+
+        // Check for division by zero
+        if (totalStaked == 0) {
+            return 0;
+        }
+
+        uint256 totalRewardsPerYear = (TOTAL_REWARD_TOKENS * SECONDS_PER_DAY) / REWARD_PERIOD;
+        return (totalRewardsPerYear * 100) / totalStaked;
+    }
+
+    function getCurrentAPY() public view returns (uint256) {
+        uint256 apr = getCurrentAPR();
+        return ((1 + apr / 100) ** 365) - 1;
+    }
+}
--- test/stake.js
+++ test/stake.js
@@ -1,77 +1,185 @@
-// SPDX-License-Identifier: MIT
-const StakingRewards = artifacts.require("StakingRewards")
-const LAZI = artifacts.require("LAZI")
+const { expect } = require("chai")
+const { BN, ether, time } = require("@openzeppelin/test-helpers")
 
-contract("StakingRewards", (accounts) => {
-    let stakingRewards
-    let token
-    const alice = accounts[0]
-    const bob = accounts[1]
+const Staking = artifacts.require("Staking")
+const ERC20 = artifacts.require("LAZI")
+const ERC721 = artifacts.require("LaziName")
+
+contract("Staking", (accounts) => {
+    const [owner, user1, user2] = accounts
+    let staking, erc20, erc721
 
     beforeEach(async () => {
-        token = await LAZI.new()
-        stakingRewards = await StakingRewards.new(token.address)
-        await token.mint(alice, 1000000)
-        await token.mint(bob, 1000000)
-        await token.approve(stakingRewards.address, 1000000, { from: alice })
-        await token.approve(stakingRewards.address, 1000000, { from: bob })
+        erc20 = await ERC20.new({ from: owner })
+        erc721 = await ERC721.new({ from: owner })
+
+        staking = await Staking.new(erc20.address, erc721.address, { from: owner })
+
+        await erc20.mint(user1, ether("500"), { from: owner })
+        await erc20.mint(user2, ether("500"), { from: owner })
+        await erc20.mint(staking.address, ether("200000000"), { from: owner })
+
+        await erc721.airdrop([user1, user1, user1], ["1 one", "2 two", "3 three"], { from: owner })
     })
 
-    it("should stake and unstake tokens", async () => {
-        await stakingRewards.stake(100, { from: alice })
-        assert.equal(await stakingRewards.totalStaked(), 100)
-        assert.equal(await stakingRewards.stakedBalances(alice), 100)
-        assert.equal(await token.balanceOf(stakingRewards.address), 100)
-        assert.equal(await token.balanceOf(alice), 999900)
+    it("should stake ERC20 tokens and ERC721 tokens", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
 
-        await stakingRewards.withdraw(50, { from: alice })
-        assert.equal(await stakingRewards.totalStaked(), 50)
-        assert.equal(await stakingRewards.stakedBalances(alice), 50)
-        assert.equal(await token.balanceOf(stakingRewards.address), 50)
-        assert.equal(await token.balanceOf(alice), 999950)
-    })
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
 
-    it("should distribute rewards to stakers", async () => {
-        // Start rewards
-        await stakingRewards.startRewards({ from: alice })
-        // assert.equal(
-        //     await token.balanceOf(stakingRewards.address),
-        //     137000 * 1461
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        const stakeInfo = await staking.stakes(user1)
+
+        // console.log("Staked ERC20 Amount:", stakeInfo.erc20Amount.toString())
+        // console.log(
+        //     "Staked ERC721 IDs:",
+        //     stakeInfo.erc721Ids.map((id) => id.toString())
         // )
-        // assert.equal(await token.balanceOf(alice), 1000000 - 137000 * 1461)
+        console.log(
+            "Staked ERC721 IDs:",
+            Object.keys(stakeInfo)
+        )
+        console.log(
+            "Staked ERC721 IDs:",
+            stakeInfo
+        )
+    })
 
-        // Alice stakes 100 tokens
-        await stakingRewards.stake(100, { from: alice })
-        assert.equal(await stakingRewards.rewardsEarned(alice), 0)
+    // Add remaining test cases for unstake, harvestRewards, compoundRewards, and other functions
 
-        // Bob stakes 200 tokens
-        await stakingRewards.stake(200, { from: bob })
-        assert.equal(await stakingRewards.rewardsEarned(alice), 0)
-        assert.equal(await stakingRewards.rewardsEarned(bob), 0)
+    it("should unstake ERC20 tokens and ERC721 tokens", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
 
-        // Wait for 1 day
-        await new Promise((resolve) => setTimeout(resolve, 24 * 60 * 60 * 1000))
-        await stakingRewards.getReward({ from: alice })
-        await stakingRewards.getReward({ from: bob })
-        assert.equal(
-            await stakingRewards.rewardsEarned(alice),
-            (100 * 137000) / 300
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        // Fast forward 30 days to make sure the staking period has passed
+        await time.increase(time.duration.days(30))
+
+        await staking.unstake({ from: user1 })
+
+        const stakeInfo = await staking.stakes(user1)
+
+        console.log(
+            "Unstaked ERC721 IDs:",
+            stakeInfo
         )
-        assert.equal(
-            await stakingRewards.rewardsEarned(bob),
-            (200 * 137000) / 300
-        )
-        assert.equal(
-            await token.balanceOf(stakingRewards.address),
-            137000 * 1460
-        )
-        assert.equal(
-            await token.balanceOf(alice),
-            1000000 - 137000 * 1460 + (100 * 137000) / 300
-        )
-        assert.equal(
-            await token.balanceOf(bob),
-            1000000 - 137000 * 1460 + (200 * 137000) / 300
-        )
+    })
+
+    it("should harvest rewards", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        // Fast forward 30 days to make sure the staking period has passed
+        await time.increase(time.duration.days(30))
+
+        await staking.harvestRewards({ from: user1 })
+
+        const stakeInfo = await staking.stakes(user1)
+
+        console.log("Claimed Rewards:", stakeInfo.claimedRewards.toString())
+    })
+
+    it("should compound rewards", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        // Fast forward 30 days to make sure the staking period has passed
+        await time.increase(time.duration.days(30))
+
+        await staking.compoundRewards({ from: user1 })
+
+        const stakeInfo = await staking.stakes(user1)
+
+        const apy = await staking.getCurrentAPY()
+        console.log("Current APY:", apy.toString())
+        
+        console.log("Compounded ERC20 Amount:", stakeInfo.erc20Amount.toString())
+    })
+
+    it("should get user rewards", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        // Fast forward 30 days to make sure the staking period has passed
+        await time.increase(time.duration.days(30))
+
+        const userRewards = await staking.getUserRewards(user1)
+
+        console.log("User Rewards:", userRewards.toString())
+    })
+
+    it("should get current APY", async () => {
+        const apy = await staking.getCurrentAPY()
+        console.log("Current APY:", apy.toString())
+    })
+
+    it("should get lock period distribution", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        const lockPeriodDistribution = await staking.getLockPeriodDistribution(30)
+        console.log("Lock Period Distribution:", lockPeriodDistribution.toString())
+    })
+
+    it("should get staked tokens distribution", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        const stakedTokensDistribution = await staking.getStakedTokensDistribution(30)
+        console.log("Staked Tokens Distribution:", stakedTokensDistribution.toString())
+    })
+
+    it("should get reward tokens distribution", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        // Fast forward 30 days to make sure the staking period has passed
+        await time.increase(time.duration.days(30))
+
+        await staking.harvestRewards({ from: user1 })
+
+        const rewardTokensDistribution = await staking.getRewardTokensDistribution(30)
+        console.log("Reward Tokens Distribution:", rewardTokensDistribution.toString())
+    })
+
+    it("should get APY distribution", async () => {
+        await erc20.approve(staking.address, ether("100"), { from: user1 })
+
+        const erc721Ids = [1, 2, 3]
+        await erc721.setApprovalForAll(staking.address, true, { from: user1 })
+
+        await staking.stake(ether("100"), 30, erc721Ids, { from: user1 })
+
+        const apyDistribution = await staking.getAPYDistribution(30)
+        console.log("APY Distribution:", apyDistribution.toString())
     })
 })
