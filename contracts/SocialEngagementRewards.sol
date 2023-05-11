eac9da321259eb96454c0b9507d8cc6b066c3895
Muneeb <muneeb.softblock@gmail.com>
2023-05-12 02:46:30 +0500

    getUserRewards, unstake penalty, REWARD_PER_DAY, REWARD_STOP_TIME

--- contracts/SocialEngagementRewards.sol
+++ contracts/SocialEngagementRewards.sol
@@ -1,8 +1,21 @@
+// TODO: soft code multiplier values
+
+// TODO: erc20.mint instead of transfer
+
+// TODO: verify contributionWeighted
+// address signer = ecrecover(timeContributionHash, v, r, s);
+// require(signer == trustedAddress, "Not signed by trusted address");
+// bytes32 expectedMessageHash = keccak256(abi.encodePacked(timestamp, contribution));
+// require(timeContributionHash == expectedMessageHash, "Message hash mismatch");
+// require(!processedValues[timeContributionHash], "Time Contribution Hash has been processed already");
+// processedValues[timeContributionHash] = true;
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -49,8 +62,8 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
     uint256 public w2 = 25;
     uint256 public w3 = 15;
 
-    uint256 public REWARD_PERIOD = 4 * 365 days;
-    uint256 public TOTAL_REWARD_TOKENS = 200_000_000 * 1e18;
+    uint256 public REWARD_STOP_TIME = block.timestamp + 4 * 365 days;
+    uint256 public REWARD_PER_DAY = 137_000 ether;
 
     event Staked(address indexed user, uint256 stakedLazi, uint256 stakeDuration, uint256[] erc721TokenIds);
     event Unstaked(address indexed user, uint256 stakedLazi, uint256[] erc721TokenIds);
@@ -66,25 +79,6 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
         erc721Token = IERC721(_erc721Token);
     }
 
-    // function harvest(uint256 contribution, uint timestamp, bytes32 timeContributionHash, uint8 v, bytes32 r, bytes32 s) external {
-    function harvest(uint256 contribution) external nonReentrant {
-        User storage user = users[msg.sender];
-        require(user.stakedLazi > 0, "No stake to claim rewards");
-
-        // address signer = ecrecover(timeContributionHash, v, r, s);
-        // require(signer == trustedAddress, "Not signed by trusted address");
-        // bytes32 expectedMessageHash = keccak256(abi.encodePacked(timestamp, contribution));
-        // require(timeContributionHash == expectedMessageHash, "Message hash mismatch");
-        // require(!processedValues[timeContributionHash], "Time Contribution Hash has been processed already");
-        // processedValues[timeContributionHash] = true;
-
-        uint256 reward = getUserRewards(msg.sender, contribution);
-        require(reward > 0, "No rewards to claim");
-
-        laziToken.transfer(msg.sender, reward);
-        emit RewardsClaimed(msg.sender, reward);
-    }
-
     /**
 
     @notice Stake LAZI tokens and ERC721 tokens
@@ -98,8 +92,8 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
     function stake(uint256 _stakedLazi, uint256 _stakeDuration, uint256[] memory _laziUsernameIds) external nonReentrant {
         User storage user = users[msg.sender];
         require(user.stakedLazi == 0, "Unstake first to stake again");
+        require(_stakedLazi > 0, "Staking amount must be greater than 0");
         require(_stakeDuration <= maxEngagementDays, "Stake duration exceeds maximum allowed");
-        require(_laziUsernameIds.length <= maxUserMultiplierTokens, "Too many ERC721 tokens");
 
         laziToken.transferFrom(msg.sender, address(this), _stakedLazi);
 
@@ -129,12 +123,32 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
      * @notice Unstake LAZI tokens and ERC721 tokens
      */
 
-    function unstake() external nonReentrant {
+    function unstake(uint256 contributionWeighted, uint256 totalWeightedContribution) external nonReentrant {
         User storage user = users[msg.sender];
         require(user.stakedLazi > 0, "No stake to unstake");
-        require(block.timestamp >= user.stakeStartTime + user.stakeDuration * 1 days, "Stake duration not completed");
 
-        laziToken.transfer(msg.sender, user.stakedLazi);
+        uint256 reward = getUserRewards(msg.sender, contributionWeighted, totalWeightedContribution);
+
+        uint256 completedDurationPercentage = ((block.timestamp - user.stakeStartTime) * 100) / user.stakeDuration;
+        uint256 stakedPenalty;
+        uint256 rewardPenalty;
+
+        if (completedDurationPercentage < 50) {
+            stakedPenalty = (user.stakedLazi * 30) / 100;
+            rewardPenalty = (reward * 50) / 100;
+        } else if (completedDurationPercentage >= 50 && completedDurationPercentage < 80) {
+            stakedPenalty = (user.stakedLazi * 15) / 100;
+            rewardPenalty = (reward * 25) / 100;
+        } else if (completedDurationPercentage >= 80 && completedDurationPercentage < 100) {
+            stakedPenalty = (user.stakedLazi * 5) / 100;
+            rewardPenalty = (reward * 15) / 100;
+        }
+
+        laziToken.transfer(msg.sender, user.stakedLazi - stakedPenalty + reward - rewardPenalty);
+        if (completedDurationPercentage < 100) {
+            laziToken.transfer(owner(), (stakedPenalty / 2) + (rewardPenalty / 2));
+            REWARD_PER_DAY += (stakedPenalty / 2) + (rewardPenalty / 2);
+        }
 
         for (uint256 i = 0; i < user.erc721TokenIds.length; i++) {
             erc721Token.transferFrom(address(this), msg.sender, user.erc721TokenIds[i]);
@@ -146,6 +160,7 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
         totalWeightedStakedDuration -= user.stakeDurationWeighted;
         totalUsers -= 1;
 
+        emit RewardsClaimed(msg.sender, reward - rewardPenalty);
         emit Unstaked(msg.sender, user.stakedLazi, user.erc721TokenIds);
         delete users[msg.sender];
     }
@@ -160,7 +175,6 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
         uint256 T = (user.stakeDuration * 1e18) / totalWeightedStakedDuration;
         uint256 U;
 
-        // TODO: soft code multiplier values
         uint erc721Tokens = user.erc721TokenIds.length;
         if (erc721Tokens == 0) {
             U = 1.00 * 1e18;
@@ -185,17 +199,20 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
      * @return The reward value
      */
 
-    function getUserRewards(address _user, uint256 contribution) public view returns (uint256) {
+    function getUserRewards(address _user, uint256 contributionWeighted, uint256 totalWeightedContribution) public view returns (uint256) {
         User storage user = users[_user];
-        uint256 elapsedTime = block.timestamp - user.stakeStartTime;
+        uint checkPoint = Math.min(block.timestamp, REWARD_STOP_TIME);
 
-        uint256 rate = TOTAL_REWARD_TOKENS / REWARD_PERIOD;
-        uint256 reward = elapsedTime * rate;
+        if (checkPoint <= user.stakeStartTime) return 0;
 
-        uint256 stakedDuration = user.stakeDurationWeighted / totalWeightedStakedDuration;
-        uint256 stakedAmount = user.stakedLaziWeighted / totalWeightedStakedLazi;
+        uint256 elapsedTime = checkPoint - user.stakeStartTime;
+        uint256 reward = elapsedTime * REWARD_PER_DAY;
 
-        uint256 totalReward = contribution * reward * w1 + stakedDuration * reward * w2 + stakedAmount * reward * w3;
+        uint256 rewardContribution = (contributionWeighted * reward * w1) / (100 * totalWeightedContribution * 1 days);
+        uint256 rewardStakedDuration = (user.stakeDurationWeighted * reward * w2) / (100 * totalWeightedStakedDuration * 1 days);
+        uint256 rewardStakedAmount = (user.stakedLaziWeighted * reward * w3) / (100 * totalWeightedStakedLazi * 1 days);
+
+        uint totalReward = rewardContribution + rewardStakedDuration + rewardStakedAmount;
         return totalReward;
     }
 
@@ -213,12 +230,12 @@ contract LaziEngagementRewards is Ownable, ERC721Holder, ReentrancyGuard {
         w3 = _w3;
     }
 
-    function set_REWARD_PERIOD(uint256 period) external onlyOwner {
-        REWARD_PERIOD = period;
+    function set_REWARD_PER_DAY(uint256 _REWARD_PER_DAY) external onlyOwner {
+        REWARD_PER_DAY = _REWARD_PER_DAY;
     }
 
-    function set_TOTAL_REWARD_TOKENS(uint256 totalTokens) external onlyOwner {
-        TOTAL_REWARD_TOKENS = totalTokens;
+    function set_REWARD_STOP_TIME(uint256 _REWARD_STOP_TIME) external onlyOwner {
+        REWARD_STOP_TIME = _REWARD_STOP_TIME;
     }
 
     function set_trustedAddress(address _trustedAddress) external onlyOwner {
