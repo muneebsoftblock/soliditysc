9573f206751a3943c309ee9cb30508dfd9bad089
Muneeb <muneeb.softblock@gmail.com>
2023-05-25 17:23:12 +0500

    file name, test cases 9/9 passing

--- contracts/SocialEngagementRewards.sol
+++ contracts/SocialEngagementRewards.sol
@@ -5,7 +5,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "./laziToken.sol";
+import "./LaziToken.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
